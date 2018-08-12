040b7e7448f2a8c101f5c8033c91f42825d5ecb4
jacotabio <32643675+jacotabio@users.noreply.github.com>
2018-08-12 20:12:58 +0800

    Ckeditor plugin import issue

--- admin/remoteTeam.php
+++ admin/remoteTeam.php
@@ -5,177 +5,203 @@ $s = (isset($_GET['s']) && $_GET['s'] != '') ? $_GET['s'] : '';
 
 $remoteTeamList = remote_team()->list("isDeleted='0'");
 
-function formatDate($val){
-  $date = date_create($val);
-  return date_format($date, "F d, Y g:i A");
+function formatDate($val)
+{
+    $date = date_create($val);
+    return date_format($date, "F d, Y g:i A");
 }
 
 ?>
-  <div class="row">
+<div class="row">
     <div class="col-sm-12">
-     <br>
-    <div class="pull-right">
-      <button type="button" class="btn btn-primary waves-effect waves-light btn-sm" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus"></i> Add New</button>
-
-    </div>
-    <br>
-    <br>
-    <?php if($message){?>
-        <div class="alert alert-success alert-dismissible fade in" role="alert">
-            <button type="button" class="close" data-dismiss="alert"
-                    aria-label="Close">
-                <span aria-hidden="true">&times;</span>
+        <br>
+        <div class="pull-right">
+            <button type="button" class="btn btn-primary waves-effect waves-light btn-sm" data-toggle="modal"
+                    data-target="#myModal"><i class="fa fa-plus"></i> Add New
             </button>
-            <?=$message;?>
+
         </div>
-      <?php }?>
-      <div class="card-box table-responsive">
-        <h4 class="page-title">Remote Team</h4><br>
-        <table id="datatable" class="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Content</th>
-              <th>Posted Date</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
+        <br>
+        <br>
+        <?php if ($message) { ?>
+            <div class="alert alert-success alert-dismissible fade in" role="alert">
+                <button type="button" class="close" data-dismiss="alert"
+                        aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                <?= $message; ?>
+            </div>
+        <?php } ?>
+        <div class="card-box table-responsive">
+            <h4 class="page-title">Remote Team</h4><br>
+            <table id="datatable" class="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Content</th>
+                    <th>Posted Date</th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
 
-           <?php foreach($remoteTeamList as $row) {
+                <?php foreach ($remoteTeamList as $row) {
 
-            if ($row->isDeleted==0){
-              $id = $row->Id;
-              ?>
-              <tr>
-                <td><?=$row->title;?></td>
-                <td style="word-break:break-all;"><?=$row->content;?></td>
-                <td><?=formatDate($row->createDate);?></td>
-                <td>
-                  <a href="#" data-toggle="modal" data-target="#update-account-modal-<?=$row->Id?>" class=" btn btn-info btn-xs" title="Click To View"  data-trigger="hover" data-toggle="tooltip"><span class="fa fa-pencil"></span> Edit</a>
-                </td>
-                <td>
-                  <a href="process.php?action=removeRemoteTeam&Id=<?=$row->Id;?>"  class=" btn btn-danger btn-xs tooltips" title="Click To Edit"><span class="fa fa-close"></span>Remove</a>
-                </td>
-              </tr>
-          <?php
-              }
-            }
-            ?>
-          </tbody>
-        </table>
-      </div>
+                    if ($row->isDeleted == 0) {
+                        $id = $row->Id;
+                        ?>
+                        <tr>
+                            <td><?= $row->title; ?></td>
+                            <td style="word-break:break-all;"><?= $row->content; ?></td>
+                            <td><?= formatDate($row->createDate); ?></td>
+                            <td>
+                                <a href="#" data-toggle="modal" data-target="#update-account-modal-<?= $row->Id ?>"
+                                   class=" btn btn-info btn-xs" title="Click To View" data-trigger="hover"
+                                   data-toggle="tooltip"><span class="fa fa-pencil"></span> Edit</a>
+                            </td>
+                            <td>
+                                <a href="process.php?action=removeRemoteTeam&Id=<?= $row->Id; ?>"
+                                   class=" btn btn-danger btn-xs tooltips" title="Click To Edit"><span
+                                            class="fa fa-close"></span>Remove</a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                }
+                ?>
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
+</div>
 </div>
 
 
 <!-- sample modal content -->
 <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-        <h4 class="modal-title" id="myModalLabel">Add New Remote Team</h4>
-      </div>
-      <div class="modal-body">
-        <form id="default-wizard" action="process.php?action=addRemoteTeam" method="POST" enctype="multipart/form-data">
-          <p class="m-b-0">
-            <?=$error?>
-          </p>
-          <div class="row m-t-20">
-            <div class="col-sm-12">
-              <div class="form-group">
-                <label>Title</label>
-                <input type="text" class="form-control" name="title" placeholder="">
-              </div>
-
-              <div class="form-group">
-              <label>Content</label>
-              <textarea id="message" class="form-control" name="content"
-                                data-parsley-trigger="keyup" data-parsley-minlength="20"
-                                data-parsley-minlength-message="Come on! You need to enter at least a 20 character comment.."
-                                data-parsley-validation-threshold="10"></textarea>
-              </div>
-
-              <div class="form-group">
-                <label>Upload Image</label>
-                <input type="file" class="form-control" name="upload_file" placeholder="" accept=".png, .jpg, .jpeg">
-                <span class="help-block"><small>Supported File: .png, .jpg, .jpeg</small></span>
-              </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Add New Remote Team</h4>
             </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn btn-primary stepy-finish">Add Remote Team</button>
-          </div>
-        </form>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+            <div class="modal-body">
+                <form id="default-wizard" action="process.php?action=addRemoteTeam" method="POST"
+                      enctype="multipart/form-data">
+                    <p class="m-b-0">
+                        <?= $error ?>
+                    </p>
+                    <div class="row m-t-20">
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <label>Title</label>
+                                <input type="text" class="form-control" name="title" placeholder="">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Content</label>
+                                <textarea id="message" class="form-control" name="content"
+                                          data-parsley-trigger="keyup" data-parsley-minlength="20"
+                                          data-parsley-minlength-message="Come on! You need to enter at least a 20 character comment.."
+                                          data-parsley-validation-threshold="10"></textarea>
+                            </div>
+
+                            <div class="form-group">
+                                <label>Upload Image</label>
+                                <input type="file" class="form-control" name="upload_file" placeholder=""
+                                       accept=".png, .jpg, .jpeg">
+                                <span class="help-block"><small>Supported File: .png, .jpg, .jpeg</small></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary stepy-finish">Add Remote Team</button>
+                    </div>
+                </form>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 </div>
 
-<?php foreach ($remoteTeamList as $row) {?>
-  <div id="update-account-modal-<?=$row->Id;?>" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
-      <div class="modal-dialog modal-lg">
-          <div class="modal-content">
-              <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                  <h4 class="modal-title" id="myLargeModalLabel">Update Remote Team</h4>
-              </div>
-              <div class="modal-body">
-                <form id="default-wizard" action="process.php?action=updateRemoteTeam" method="POST" enctype="multipart/form-data">
-                   <p class="m-b-0">
-                      <?=$error?>
-                  </p>
-                  <input type="hidden" name="Id" value="<?=$row->Id;?>">
-                  <div class="row m-t-20">
-                    <div class="col-sm-12">
-                      <div class="form-group">
-                        <label>Title</label>
-                        <input type="text" class="form-control" name="title" value="<?=$row->title;?>">
-                      </div>
+<?php foreach ($remoteTeamList as $row) { ?>
+    <div id="update-account-modal-<?= $row->Id; ?>" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog"
+         aria-labelledby="myLargeModalLabel" aria-hidden="true" style="display: none;">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title" id="myLargeModalLabel">Update Remote Team</h4>
+                </div>
+                <div class="modal-body">
+                    <form id="default-wizard" action="process.php?action=updateRemoteTeam" method="POST"
+                          enctype="multipart/form-data">
+                        <p class="m-b-0">
+                            <?= $error ?>
+                        </p>
+                        <input type="hidden" name="Id" value="<?= $row->Id; ?>">
+                        <div class="row m-t-20">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label>Header Image</label>
+                                    <input type="file" class="form-control" name="header_image"
+                                           value="<?= $row->headerImage; ?>" accept=".png, .jpg, .jpeg">
+                                    <span class="help-block"><small>Supported File: .png, .jpg, .jpeg</small></span>
+                                    <span><a href="../media/<?= $row->headerImage; ?>" target="blank_">Click to view image</a></span>
+                                </div>
+                                <div class="form-group">
+                                    <label>Title</label>
+                                    <input type="text" class="form-control" name="title" value="<?= $row->title; ?>">
+                                </div>
 
-                      <div class="form-group">
-                      <label>Content</label>
-                      <textarea class="form-control textarea-wysiwyg" name="content"
-                                        data-parsley-trigger="keyup" data-parsley-minlength="20"
-                                        data-parsley-minlength-message="Come on! You need to enter at least a 20 character comment.."
-                                        data-parsley-validation-threshold="10"><?=$row->content;?></textarea>
-                      </div>
+                                <div class="form-group">
+                                    <label>Content</label>
+                                    <textarea class="form-control textarea-wysiwyg" name="content"
+                                              data-parsley-trigger="keyup" data-parsley-minlength="20"
+                                              data-parsley-minlength-message="Come on! You need to enter at least a 20 character comment.."
+                                              data-parsley-validation-threshold="10"><?= $row->content; ?></textarea>
+                                </div>
 
-                      <div class="form-group">
-                        <label>Upload Image</label>
-                        <input type="file" class="form-control" name="upload_file" value="<?=$row->uploadedImage;?>" accept=".png, .jpg, .jpeg">
-                        <span class="help-block"><small>Supported File: .png, .jpg, .jpeg</small></span>
-                        <span><a href="../media/<?=$row->uploadedImage;?>" target="blank_">Click to view image</a></span>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default waves-effect btn-sm" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary stepy-finish btn-sm">Update Remote Team</button>
-                  </div>
-                </form>
-              </div>
-          </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+                                <div class="form-group">
+                                    <label>Upload Image</label>
+                                    <input type="file" class="form-control" name="upload_file"
+                                           value="<?= $row->uploadedImage; ?>" accept=".png, .jpg, .jpeg">
+                                    <span class="help-block"><small>Supported File: .png, .jpg, .jpeg</small></span>
+                                    <span><a href="../media/<?= $row->uploadedImage; ?>" target="blank_">Click to view image</a></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default waves-effect btn-sm" data-dismiss="modal">
+                                Close
+                            </button>
+                            <button type="submit" class="btn btn-primary stepy-finish btn-sm">Update Remote Team
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 <?php } ?>
 
 <textarea id="checking"></textarea>
-
+<script src="/teamire/@ckeditor/ckeditor5-font/src/font.js"></script>
 <script type="text/javascript">
+    //import Font from '/teamire/@ckeditor/ckeditor5-font/src/font';
+
     ClassicEditor.create(document.querySelector('#checking'));
-ClassicEditor
-    .create(document.querySelector( '.textarea-wysiwyg' ))
-    .then( editor => {
-        console.log( editor );
-    } )
-    .catch( error => {
-        console.error( error );
-    } );
+
+
+    ClassicEditor
+        .create(document.querySelector('.textarea-wysiwyg'))
+        .then(editor => {
+            console.log(editor);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 
 
 </script>
--- home/remoteTeamDetail.php
+++ home/remoteTeamDetail.php
@@ -1,11 +1,15 @@
 <?php
 $Id = (isset($_GET['Id']) && $_GET['Id'] != '') ? $_GET['Id'] : '';
 $remoteTeam = remote_team()->get("Id='$Id'");
-$firstRemoteTeam = remote_team()->list("isDeleted='0' order by `Id` limit 1");
+//$firstRemoteTeam = remote_team()->list("isDeleted='0' order by `Id` limit 1");
 ?>
 
 <div class="cms-container">
-    <div class="parallax-window" data-parallax="scroll"></div>
+    <div class="parallax-window" data-parallax="scroll">
+        <div class="">
+            <span>Sampl</span>
+        </div>
+    </div>
     <div class="cms-content">
   <?php if($remoteTeam) {?>
     <?php if($remoteTeam->uploadedImage != ""){ ?>
@@ -16,13 +20,6 @@ $firstRemoteTeam = remote_team()->list("isDeleted='0' order by `Id` limit 1");
     <h3 class="text-blue"><?=$remoteTeam->title;?></h3>
 
     <p><?= html_entity_decode(nl2br($remoteTeam->content));?></p>
-  <?php }else if($firstRemoteTeam){ ?>
-    <?php foreach($firstRemoteTeam as $row){ ?>
-      <img class="pull-right" width="30%" height="30%" src="../media/<?=$row->uploadedImage;?>">
-      <h3 class="text-blue"><?=$row->title;?></h3>
-
-      <?= html_entity_decode(nl2br($row->content));?>
-    <?php } ?>
   <?php }else{ ?>
     <h4 class="text-center text-muted" style="margin-right: 35%;"><i class="fa fa-file-text-o fa-5x"></i><br><br> New content will be coming soon </h4>
   <?php } ?>
@@ -30,6 +27,6 @@ $firstRemoteTeam = remote_team()->list("isDeleted='0' order by `Id` limit 1");
 </div>
 <script>
     if(!Modernizr.touch){
-        $('.parallax-window').parallax({imageSrc: '../media/hero-banner-sample.jpg'});
+        $('.parallax-window').parallax({imageSrc: '../media/<?=$remoteTeam->headerImage;?>'});
     }
 </script>
