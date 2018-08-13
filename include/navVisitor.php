c09fd182141369738b764238dc43743644d1053e
jacotabio <32643675+jacotabio@users.noreply.github.com>
2018-08-13 21:54:51 +0800

    Changes in navigation text

--- include/navVisitor.php
+++ include/navVisitor.php
@@ -12,8 +12,8 @@
       <ul class="nav navbar-nav navbar-right nav-menu-right" id="above-nav">
         <li> <a class="m-l-3 m-r-3" href="../home/?view=projects" style="font-size:15px;">Supply Chain Projects</a></li>
         <li> <a class="m-l-3 m-r-3" href="../home/?view=logins" style="font-size:15px;">Timesheets</a></li>
-        <li> <a class="m-l-3 m-r-3" href="../home/?view=remoteTeam" style="font-size:15px;">Your Remote Team</a></li>
-        <li> <a class="m-l-3 m-r-3" href="../home/?view=hiringForm" style="font-size:15px;">Request Staff</a></li>
+        <li> <a class="m-l-3 m-r-3" href="../home/?view=remoteTeam" style="font-size:15px;">Remote Team</a></li>
+        <li> <a class="m-l-3 m-r-3" href="../home/?view=hiringForm" style="font-size:15px;">Request Talent</a></li>
         <li><a class="m-l-3 m-r-3" href="../home/?view=contactUs" style="font-size:15px;">Contact Us</a></li>
       </ul>
     </div>
@@ -31,10 +31,10 @@
                     <label for="drop-2" class="toggle1"> <a  href="../home/?view=logins" >Timesheets</a></label>
                 </li>
                  <li>
-                    <label for="drop-2" class="toggle1"> <a  href="../home/?view=remoteTeam" >Your Remote Team</a></label>
+                    <label for="drop-2" class="toggle1"> <a  href="../home/?view=remoteTeam" >Remote Team</a></label>
                 </li>
                  <li>
-                    <label for="drop-2" class="toggle1"> <a  href="../home/?view=hiringForm" >Request Staff</a></label>
+                    <label for="drop-2" class="toggle1"> <a  href="../home/?view=hiringForm" >Request Talent</a></label>
                 </li>
                  <li>
                     <label for="drop-2" class="toggle1"> <a  href="../home/?view=contactUs" >Contact Us</a></label>
