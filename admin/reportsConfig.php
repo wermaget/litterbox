649dc8b02c538ba8763f9d81ad1cb5d93a464a3b
HL <hl.lim@protonmail.com>
2018-11-02 01:06:36 +0800

    reports looping optimization, added comments on reportConfig

--- admin/reportsConfig.php
+++ admin/reportsConfig.php
@@ -2,6 +2,7 @@
 $data = array();
 $options = ['candidate','company','inquiries','talents'];
 
+//table headers
 $_candidate = ['First Name','Last Name','Email','Mobile','Address','State','Resume'];
 $_company = ['Company Name','Description','Email','Contact Person','Phone Number','Address'];
 
@@ -10,6 +11,7 @@ $data = [
     'candidate' => [
         'headers' => $_candidate,
         'cols' => [
+            //this array can be moved outside for better readability
             'firstName', 'lastName', 'email', 'phoneNumber', 'address1', 'state', 'uploadedResume'
         ]
     ],
